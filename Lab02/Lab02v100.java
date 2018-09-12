a56d073cac2c6c2056984ceaa7b47fa19c01d088
Mason Wilderom <mason.w20@student.parishepiscopal.org>
2018-09-12 14:46:25 -0500

    added

---
+++ Lab02/Lab02v100.java
@@ -0,0 +1,22 @@
+// Lab02v80.java
+
+
+public class Lab02v100
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Lab 02, 80 Point Version");
+        System.out.println();
+        System.out.println();
+        System.out.print("Measure today's knowledge ");
+        System.out.println("by yesterday's confusion.");
+        System.out.println();
+        System.out.println("Bewilderment + Exposure = Obvious");
+        System.out.println("");
+        System.out.println("# ##### #####    ##### ##### # #   # ##### #####");
+        System.out.println("# #   # #   #    #   # #   # # ##  #   #   #");
+        System.out.println("# #   # #   #    ##### #   # # # # #   #   #####");
+        System.out.println("# #   # #   #    #     #   # # #  ##   #       #");
+        System.out.println("# ##### #####    #     ##### # #   #   #   #####");
+    }
+}
